73818b85f53d9fc527697f3d4e8880597b8e0d71
Aldi <alditjendana@gmail.com>
2022-04-19 21:04:04 +0700

    [REFACTOR]add field details in seriliazer bill

--- paytungan/app/split_bill/serializers.py
+++ paytungan/app/split_bill/serializers.py
@@ -8,7 +8,7 @@ class GetBillRequest(serializers.Serializer):
     id = serializers.IntegerField(min_value=1)
 
 
-class UserSerializer(serializers.Serializer):
+class UserProfileSerializer(serializers.Serializer):
     phone_number = serializers.CharField()
     username = serializers.CharField(
         required=False, default=None, allow_null=True, allow_blank=True
@@ -32,7 +32,7 @@ class BillSerializer(serializers.Serializer):
     amount = serializers.IntegerField(min_value=0)
     created_at = serializers.DateTimeField()
     updated_at = serializers.DateTimeField()
-    user = UserSerializer(required=False, allow_null=True)
+    user = UserProfileSerializer(required=False, allow_null=True)
     details = serializers.CharField(required=False, allow_null=True)
 
 
@@ -75,6 +75,7 @@ class GetSplitBillResponse(serializers.Serializer):
 class UserIdWithAmountBillSerializer(serializers.Serializer):
     user_id = serializers.IntegerField(min_value=1)
     amount = serializers.IntegerField(min_value=0)
+    details = serializers.CharField(required=False, allow_null=True, default=None)
 
 
 class CreateSplitBillRequest(serializers.Serializer):
